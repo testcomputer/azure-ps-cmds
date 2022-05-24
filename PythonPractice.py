dcde7dcf3823c71531322b69e24bd1819d09a291
testcomputer <104815254+testcomputer@users.noreply.github.com>
2022-05-24 04:27:13 -0600

    Create PythonPractice.py

---
+++ PythonPractice.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+x = 3              # a whole number                   
+f = 3.1415926      # a floating point number              
+name = "Python"    # a string
+
+print(x)
+print(f)
+print(name)
+
+combination = name + " " + name
+print(combination)
+
+sum = f + f
+print(sum)
+
+python example.py
+
+Less than ( < )
+Less than or equal to (<=)
+Greater than (>)
+Greater than or equal to (>=)
+Equal to ( == )
+Not equal to ( != )
+
+
+left_value  < right_value
+
+##Example##
+
+>>> 10 < 20
+True
+>>> 30 < 20
+False
+
+
