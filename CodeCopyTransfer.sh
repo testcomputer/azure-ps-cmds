6e025853fedabfcacad141eb7dbc718bbd9c4841
testcomputer <104815254+testcomputer@users.noreply.github.com>
2022-10-13 21:49:40 -0600

    Update CodeCopyTransfer.sh

--- CodeCopyTransfer.sh
+++ CodeCopyTransfer.sh
@@ -32,3 +32,11 @@ have to type in the code every day I am on the network.
 
 
 get-wmiobject win32_networkadapter | select netconnectionid, name, InterfaceIndex, netconnectionstatus
+
+
+
+get-wmiobject win32_networkadapter -filter "netconnectionstatus = 2" |
+
+select netconnectionid, name, InterfaceIndex, netconnectionstatus
+
+
