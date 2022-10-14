554db3982424bbff823d3c9a01a2907b179f8803
testcomputer <104815254+testcomputer@users.noreply.github.com>
2022-10-13 19:53:00 -0600

    Create CodeCopy.sh

---
+++ CodeCopy.sh
@@ -0,0 +1,33 @@
+
+Get-CimInstance -ClassName Win32_BIOS
+
+-SMBIOSBIOSVersion
+-Manufacturer 
+-Name         
+-SeriallNumber
+-Version
+
+
+Get-DnsClientCache
+
+This command will list out the DNS Cache Entries.
+
+[] ipconfig /displaydns > dnscacheentries.txt
+
+[] ipconfig /flushdns
+
+What is the big deal with wireless networks?
+
+Wireless auto-connect is a great feature for a laptop 
+that never leaves trusted premises. So, if my laptop 
+only follows me to the Microsoft office and back to 
+my house, having wireless auto-connect is great. The 
+problem is that I travel in wider circles than that. 
+I may, for example, spend a week in a Marriott Hotel 
+in Seattle, and then spend a few days in a Hyatt in Atlanta, 
+and then slide on into a Hilton in Savannah. While I am at the hotel, 
+I generally connect to the wireless network (here is where the Windows Firewall 
+and network connection profiles come in handy). Therefore, it is irritating to 
+have to type in the code every day I am on the network.
+
+
